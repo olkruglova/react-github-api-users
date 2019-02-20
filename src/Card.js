08bb594c6c6075a8b24769f02e967da71c8e2f66
olkruglova <olkruhlova@gmail.com>
2019-02-20 15:10:56 +0100

    Install axios, add gitHub api and form for submit a githubName

---
+++ src/Card.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './Card.css';
+
+const Card = (props) => {
+  return (
+    <div style={{margin: '1em'}}>
+      <img width="75" src={props.avatar_url} />
+      <div className="info" style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}> {props.name}</div>
+        <div>{props.company}</div>
+      </div>
+    </div>
+
+  );
+};
+
+
+class Form extends React.Component {
+  state = { userName: '' }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submited!', this.state.userName);
+
+    // ajax...(fetch or axios)
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    .then(resp => {
+      this.props.onSubmit(resp.data);
+      this.setState({ userName: ''})
+    })
+  }
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder="Github username" required />
+        <button type="submit">Add card</button>
+      </form>
+    )
+  }
+}
+
+const CardList = (props) => {
+  return (
+    <div>
+      {props.cards.map(card => <Card key={card.id} {...card} />)}
+    </div>
+  )
+}
+
+class App extends React.Component {
+
+  state = {
+    cards: []
+  };
+
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(cardInfo)
+    }));
+  };
+
+  render () {
+    return (
+      <div>
+        <Form onSubmit={this.addNewCard}/>
+        <CardList cards={this.state.cards}/>
+      </div>
+    )
+  }
+}
+
+export default App;
